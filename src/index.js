2d6b3a8e25eb69f512e217842991bc33469a485b
bogdanovs <sergey.bogdanov@geropharm.com>
2020-04-29 01:31:40 +0300

    setting exsoress heroku

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const path = require('path');
 const app = express();
+require('process');
+require('dotenv').config();
 
-require('./mongo');
+// require('./mongo');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true });
 
 const { findOneBook } = require('./mongo/book-fn');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/', function(req, res) {
+  res.status(200).json({ success: true, message: 'Hello itmo!' });
 })
 
-app.get('/book/:name', async function(request, response) {
-  response.send(await findOneBook(request.params.name));
+app.get('/book/:name', async function(req, res) {
+  res.send(await findOneBook(request.params.name));
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,4 +1,6 @@
+require('dotenv');
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mvc-mongo', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(progress.env.MONGODB_URL || 'mongodb://localhost:27017/mvc-mongo', {useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
