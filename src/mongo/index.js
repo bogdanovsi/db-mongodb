d534782fad24ed22a5060640b6ce50327fb40e76
bogdanovs <sergey.bogdanov@geropharm.com>
2020-04-30 19:05:55 +0300

    test dep

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,6 +1,8 @@
 require('dotenv');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mvc-mongo', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mvc-mongo', {useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Database Connected Successfully"))
+.catch(err => console.log(err));
 
 module.exports = mongoose;
