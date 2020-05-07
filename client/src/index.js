1a11f1c90fbace94aa74064afbdf2ab8da5ed7de
bogdanovs <sergey.bogdanov@geropharm.com>
2020-05-07 22:09:58 +0300

    setup webpack

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+const ReactDOM = require('react-dom');
+const App = require('./App');
+
+ReactDOM.render(App, document.getElementById('root'));
---
+++ client/webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: { 
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
