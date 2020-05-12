28c9dcf88094d7f9ad0b71486e668c13999e12cc
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2020-05-12 21:54:34 +0300

    -history

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import history from '../history';
 import MongoInput from './MongoInput';
 import Menu from './Menu';
 import RouterList from './RouterList';
