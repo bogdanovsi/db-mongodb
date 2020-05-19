e392f2cf60b901fcee139e29f8271a75b8a853bb
bogdanovs <sergey.bogdanov@geropharm.com>
2020-05-19 18:34:42 +0300

    added books

--- server/src/routers/contracts.js
+++ server/src/routers/contracts.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const MongooseCore = require('./mongoose-core');
-
-const { Contract } = require('../models');
+const mongoose = require('mongoose');
+const { Contract, Book } = require('../models');
 
 const possibleKeys = [
     "_id"
@@ -30,6 +30,45 @@ router.get('/all', async (req, res) => {
         console.log(err);
     });
 })
+
+const processingContract = async (contract) => {        
+    const bookInContract = await Book.aggregate([
+        {
+            $lookup: {
+                from: "writers",
+                localField: 'writer',
+                foreignField: "_id",
+                as: "writer"
+            }
+        }
+    ]).match({ 
+        'writer._id': mongoose.Types.ObjectId(contract.writer[0]._id),
+        publication_date: {$gte: new Date(contract.created), $lt: new Date(contract.expiration_date)}
+    })
+    .limit(1)
+
+    return {
+        data: contract,
+        books: bookInContract
+    }
+}
+
+router.get('/data/:contractId', async (req, res) => {
+    const results = await Contract.aggregate([
+        {
+            $lookup: {
+                from: "writers",
+                localField: 'writer',
+                foreignField: "_id",
+                as: "writer"
+            }
+        }
+    ])
+    .match({ _id: mongoose.Types.ObjectId(req.params.contractId) })
+    .limit(1)
+
+    res.send(results[0] ? await processingContract(results[0]) : null);
+});
 
 router.get('/:name', async function(req, res) {
     res.send(await Contract.findOneBook(req.params.name));
