8b316ade44c2f55f76a34ea075f4b62562ec4d7c
bogdanovs <sergey.bogdanov@geropharm.com>
2020-05-19 20:00:52 +0300

    added books

--- server/src/routers/writers.js
+++ server/src/routers/writers.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const MongooseCore = require('./mongoose-core');
-const { Writer } = require('../models');
+const { Writer, Book } = require('../models');
+const mongoose = require('mongoose');
 
 const possibleKeys = [
     "_id"
@@ -12,6 +13,23 @@ MongooseCore.bindDefaultDeleteAll(router, '/all', Writer);
 MongooseCore.bindDefaultCreateModel(router, '/', Writer);
 MongooseCore.bindDefaultDeleteByKeys(router, '/', Writer, possibleKeys);
 MongooseCore.bindUpdateModel(router, Writer);
+
+router.get('/:writerId/books', async function(req, res) {
+    const books = await Book.aggregate([
+        {
+            $lookup: {
+                from: "writers",
+                localField: 'writer',
+                foreignField: "_id",
+                as: "writer"
+            }
+        }
+    ]).match({ 
+        'writer._id': mongoose.Types.ObjectId(req.params.writerId)
+    });
+
+    res.send(books);
+})
 
 router.get('/:name', async function(req, res) {
     res.send(await Writer.find({ name: req.params.name }));
